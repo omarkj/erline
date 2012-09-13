4f81b5d43e9e054954922be01bd943a949b107e3
Ómar Kjartan Yasin <omarkj@gmail.com>
2012-09-13 14:55:47 +0000

    Added some documentation

--- src/erline.erl
+++ src/erline.erl
@@ -4,6 +4,16 @@
 	 async/2]).
 -include("erline.hrl").
 
+-export_type([pipeline/0]).
+
+%% @doc
+%% Create a new pipeline. This doesn't start it, only
+%% validates it and returns a pipeline that can be run using
+%% @see sync/2. and @see async/2.
+%% @end
+-spec create(sequential|concurrent,
+	     [module()|pipeline()|function()]|[],
+	     []) -> pipeline().
 create(sequential, Actions, Options) ->
     create(#pipeline{type=sequential}, Actions, Options);
 create(concurrent, Actions, Options) ->
@@ -11,6 +21,29 @@ create(concurrent, Actions, Options) ->
 create(#pipeline{}=Pipeline, Actions, Options) ->
     [Pipeline#pipeline{actions=validate_actions(Actions, []),
 		       opts=Options}].
+
+%% @doc
+%% Runs the pipeline and wait for it to finish.
+%% @end
+-spec sync(pipeline(), any()) -> [any()].
+sync(Pipelines, Input) ->
+    Ref = async(Pipelines, Input),
+    receive
+	{Ref, Res} ->
+	    Res
+    end.
+
+%% @doc
+%% Runs the pipeline asyncly, returns a reference that
+%% will be returned to the calling process in a tuple
+%% looking like this
+%% ```{Ref, Results}'''
+%% @TODO monitor to the running manager to be able to get
+%% a notification if it crashes during the run.
+%% @end
+-spec async(pipeline(), any()) -> reference().
+async([#pipeline{}|_]=Pipelines, Input) ->
+    erline_manager_sup:start_pipeline(self(), Pipelines, Input).
 
 validate_actions([], Res) ->
     Res;
@@ -23,14 +56,3 @@ validate_actions([Module|Rest], Res) when is_atom(Module) ->
     end;
 validate_actions([[#pipeline{}|_]=Pipelines|Rest], Res) ->
     validate_actions(Rest, Res++[Pipelines]).
-
-sync(Pipelines, Input) ->
-    Ref = async(Pipelines, Input),
-    receive
-	{Ref, Res} ->
-	    Res
-    end.
-
-async([#pipeline{}|_]=Pipelines, Input) ->
-    erline_manager_sup:start_pipeline(self(), Pipelines, Input).
-
--- src/erline.hrl
+++ src/erline.hrl
@@ -1,14 +1,5 @@
 -type pipeline_type() :: parallel|sequential.
-
--type pipeline() :: {pipeline_type(),
-		     [module()]|[pipeline()]|[],
-		     pipeline(),
-		     {finally, pipeline()}}|
-		    {pipeline_type(),
-		     [module()]|[pipeline()]|[],
-		     pipeline()}.
-		    
 -record(pipeline, {type :: pipeline_type(),
 		   opts :: term()|undefined,
-		   actions = [] :: []|[module()]|[pipeline()]|
-				   maybe_improper_list(module(),pipeline())}).
+		   actions = [] :: [module()|[#pipeline{}]|function()]|[]}).
+-type pipeline() :: list(#pipeline{}).
